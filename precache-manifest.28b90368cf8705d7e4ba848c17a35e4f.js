b1d457ad69731601c38f5b393b31136ae4a95f55
Sorrab <ramonpbarros@gmail.com>
2020-06-16 16:49:34 -0700

    Updates

---
+++ precache-manifest.28b90368cf8705d7e4ba848c17a35e4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2c0ec3fa709af766bee133a77386b8",
+    "url": "/student-chart/index.html"
+  },
+  {
+    "revision": "299b2abf57ea25bce7df",
+    "url": "/student-chart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "12d4d93d34354e5305a1",
+    "url": "/student-chart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "299b2abf57ea25bce7df",
+    "url": "/student-chart/static/js/2.4e427166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/student-chart/static/js/2.4e427166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d4d93d34354e5305a1",
+    "url": "/student-chart/static/js/main.b8e917e8.chunk.js"
+  },
+  {
+    "revision": "66fdda55daa4ac0e55a8",
+    "url": "/student-chart/static/js/runtime-main.72125906.js"
+  }
+]);
